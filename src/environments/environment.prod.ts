643b0b0bbdf708ab3c1c291f7b099ddb7b7a66a2
Angel Mora <angelmora13@hotmail.com>
2020-11-12 15:46:33 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
-  firestore:{
-    apiKey: "AIzaSyC-e3ZiYr70OntjkjJun6WpzAPhe0bqa8w",
-    authDomain: "formulador-1d40b.firebaseapp.com",
-    databaseURL: "https://formulador-1d40b.firebaseio.com",
-    projectId: "formulador-1d40b",
-    storageBucket: "formulador-1d40b.appspot.com",
-    messagingSenderId: "1059523185928",
-    appId: "1:1059523185928:web:3894d5cac77035e65930bd",
-    measurementId: "G-Q00BJJSPZD"
-  }
+
+
 };
